eac112bdacf13647e68a0f3c86bb7b4daa9c1d74
Vladimir Vinnikov <vladvinnikov@gmail.com>
2011-11-30 19:59:21 +0200

    -Add hook to blog

--- lib/spree_essential_blog/custom_hooks.rb
+++ lib/spree_essential_blog/custom_hooks.rb
@@ -3,6 +3,10 @@ module SpreeEssentialBlog
   class CustomHooks < Spree::ThemeSupport::HookListener
 
     insert_after :admin_configurations_menu, 'blog/admin/configurations/disqus_config'
+    
+      insert_before :sidebar do
+        "<li><%= link_to t('admin.shared.contents_tab.content'), posts_url %></li>"
+      end
   
   end
   
